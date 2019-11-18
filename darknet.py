355c68773750aea9c483f81d9d3e9800f1ecccaa
leonmayer <39220957+leonmayer@users.noreply.github.com>
2019-11-18 09:35:07 +0100

    Add numpy support

--- darknet.py
+++ darknet.py
@@ -226,17 +226,47 @@ def classify(net, meta, im):
     res = sorted(res, key=lambda x: -x[1])
     return res
 
-def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45, debug= False):
-    """
-    Performs the meat of the detection
-    """
-    #pylint: disable= C0321
-    im = load_image(image, 0, 0)
-    if debug: print("Loaded image")
-    ret = detect_image(net, meta, im, thresh, hier_thresh, nms, debug)
-    free_image(im)
-    if debug: print("freed image")
-    return ret
+def array_to_image(arr):
+    # need to return old values to avoid python freeing memory
+    arr = arr.transpose(2,0,1)
+    c, h, w = arr.shape[0:3]
+    arr = np.ascontiguousarray(arr.flat, dtype=np.float32) / 255.0
+    data = arr.ctypes.data_as(POINTER(c_float))
+    im = IMAGE(w,h,c,data)
+    return im, arr
+
+def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    if isinstance(image, bytes):  
+        # image is a filename 
+        # i.e. image = b'/darknet/data/dog.jpg'
+        im = load_image(image, 0, 0)
+    else:  
+        # image is an nparray
+        # i.e. image = cv2.imread('/darknet/data/dog.jpg')
+        im, image = array_to_image(image)
+        rgbgr_image(im)
+    num = c_int(0)
+    pnum = pointer(num)
+    predict_image(net, im)
+    dets = get_network_boxes(net, im.w, im.h, thresh, 
+                             hier_thresh, None, 0, pnum)
+    num = pnum[0]
+    if nms: do_nms_obj(dets, num, meta.classes, nms)
+
+    res = []
+    for j in range(num):
+        a = dets[j].prob[0:meta.classes]
+        if any(a):
+            ai = np.array(a).nonzero()[0]
+            for i in ai:
+                b = dets[j].bbox
+                res.append((meta.names[i], dets[j].prob[i], 
+                           (b.x, b.y, b.w, b.h)))
+
+    res = sorted(res, key=lambda x: -x[1])
+    if isinstance(image, bytes): free_image(im)
+    free_detections(dets, num)
+    return res
 
 def detect_image(net, meta, im, thresh=.5, hier_thresh=.5, nms=.45, debug= False):
     #import cv2
