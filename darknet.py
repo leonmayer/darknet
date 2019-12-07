4e8c772e59064d808b3de1a3374bcd2eabc9bbb0
leonmayer <39220957+leonmayer@users.noreply.github.com>
2019-12-07 15:09:22 +0100

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -389,6 +389,7 @@ def performDetect(imagePath="data/dog.jpg", thresh= 0.25, configPath = "/content
             from skimage import io, draw
             import numpy as np
             image = io.imread(imagePath)
+            """
             print("*** "+str(len(detections))+" Results, color coded by confidence ***")
             imcaption = []
             for detection in detections:
@@ -426,7 +427,8 @@ def performDetect(imagePath="data/dog.jpg", thresh= 0.25, configPath = "/content
                 draw.set_color(image, (rr3, cc3), boxColor, alpha= 0.8)
                 draw.set_color(image, (rr4, cc4), boxColor, alpha= 0.8)
                 draw.set_color(image, (rr5, cc5), boxColor, alpha= 0.8)
-            if not makeImageOnly:
+            """
+            #if not makeImageOnly:
                 io.imshow(image)
                 io.show()
             detections = {
